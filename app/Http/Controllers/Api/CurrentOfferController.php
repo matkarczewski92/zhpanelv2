49b401e86644e42eca15d2408df2945c9b5477f0
Mateusz <matkarczewski92@gmail.com>
2026-02-04 22:36:37 +0100

    create

--- app/Http/Controllers/Api/CurrentOfferController.php
+++ app/Http/Controllers/Api/CurrentOfferController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Application\Api\Queries\CurrentOffersQueryService;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\Api\CurrentOfferResource;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\JsonResponse;
 
 class CurrentOfferController extends Controller
 {
-    public function index(CurrentOffersQueryService $queryService): AnonymousResourceCollection
+    public function index(CurrentOffersQueryService $queryService): JsonResponse
     {
-        return CurrentOfferResource::collection($queryService->handle());
+        return response()->json(
+            ['data' => $queryService->handle()],
+            200,
+            ['Content-Type' => 'application/json; charset=UTF-8'],
+            JSON_UNESCAPED_UNICODE | JSON_INVALID_UTF8_SUBSTITUTE
+        );
     }
 }
