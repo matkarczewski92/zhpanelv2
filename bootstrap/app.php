69183664332653a57916d796f172dd692143b250
Mateusz <matkarczewski92@gmail.com>
2026-03-26 07:23:35 +0100

    f

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,5 +18,11 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (NotFoundHttpException $exception, $request) {
+            if ($request->expectsJson()) {
+                return null;
+            }
+
+            return response()->view('errors.404', [], 404);
+        });
     })->create();
