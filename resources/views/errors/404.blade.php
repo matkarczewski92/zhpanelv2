97b760443b667cb9d929efdcc7f8d82b88eb15d3
Mateusz <matkarczewski92@gmail.com>
2026-03-26 07:24:15 +0100

    f

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -10,27 +10,19 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="landing-body">
-    <main class="landing-404">
-        <div class="container">
+    <main class="landing-404" style="--landing-404-bg: url('{{ asset('images/landing/404.png') }}');">
+        <div class="landing-404-backdrop"></div>
+        <div class="container position-relative">
             <div class="landing-404-card">
-                <div class="landing-404-copy">
-                    <span class="landing-404-code">404</span>
-                    <h1 class="landing-404-title">Tej strony nie udało się odnaleźć</h1>
-                    <p class="landing-404-text">
-                        Możliwe, że adres jest nieaktualny albo strona została przeniesiona.
-                        Wróć na stronę główną i przejdź dalej stamtąd.
-                    </p>
-                    <div class="d-flex flex-wrap gap-2 justify-content-center justify-content-lg-start">
-                        <a href="{{ url('/') }}" class="btn btn-primary">Wróć na stronę główną</a>
-                        <a href="{{ url()->previous() }}" class="btn btn-outline-light">Cofnij</a>
-                    </div>
-                </div>
-                <div class="landing-404-visual">
-                    <img
-                        src="{{ asset('images/landing/404.png') }}"
-                        alt="Ilustracja błędu 404"
-                        class="landing-404-image"
-                    >
+                <span class="landing-404-code">404</span>
+                <h1 class="landing-404-title">Tej strony nie udało się odnaleźć</h1>
+                <p class="landing-404-text">
+                    Możliwe, że adres jest nieaktualny albo strona została przeniesiona.
+                    Wróć na stronę główną i przejdź dalej stamtąd.
+                </p>
+                <div class="d-flex flex-wrap gap-2 justify-content-center">
+                    <a href="{{ url('/') }}" class="btn btn-primary">Wróć na stronę główną</a>
+                    <a href="{{ url()->previous() }}" class="btn btn-outline-light">Cofnij</a>
                 </div>
             </div>
         </div>
