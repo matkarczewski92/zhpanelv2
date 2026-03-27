34f6733e1b0eaf8f3c79f758499ddc931c480aab
Mateusz <matkarczewski92@gmail.com>
2026-03-27 21:56:10 +0100

    g

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -18,42 +18,44 @@
         </div>
     </div>
 
-    @include('admin.settings.partials.import-preview', ['importPreview' => $importPreview ?? null])
+    <div class="admin-settings-shell">
+        @include('admin.settings.partials.import-preview', ['importPreview' => $importPreview ?? null])
 
-    <ul class="nav nav-pills mb-3" id="settingsTabs" role="tablist">
-        @php $tabs = [
-            'categories' => 'Kategorie',
-            'types' => 'Typy',
-            'genes' => 'Genotyp: Kategorie',
-            'traits' => 'Genotyp: Traits',
-            'winter' => 'Zimowanie: Etapy',
-            'system' => 'System config',
-            'feeds' => 'Karma',
-            'finance-categories' => 'Kategorie finansowe',
-            'color-groups' => 'Grupy kolorystyczne',
-            'ewelink-devices' => 'eWeLink: Urządzenia',
-            'genetics-generator' => 'Generuj genetykę',
-            'update' => 'Aktualizacja',
-        ]; @endphp
-        @foreach($tabs as $key => $label)
-            <li class="nav-item" role="presentation">
-                <button class="nav-link @if($vm->activeTab === $key) active @endif" data-bs-toggle="tab" data-bs-target="#tab-{{ $key }}" type="button" role="tab">{{ $label }}</button>
-            </li>
-        @endforeach
-    </ul>
+        <ul class="nav nav-pills mb-3" id="settingsTabs" role="tablist">
+            @php $tabs = [
+                'categories' => 'Kategorie',
+                'types' => 'Typy',
+                'genes' => 'Genotyp: Kategorie',
+                'traits' => 'Genotyp: Traits',
+                'winter' => 'Zimowanie: Etapy',
+                'system' => 'System config',
+                'feeds' => 'Karma',
+                'finance-categories' => 'Kategorie finansowe',
+                'color-groups' => 'Grupy kolorystyczne',
+                'ewelink-devices' => 'eWeLink: Urządzenia',
+                'genetics-generator' => 'Generuj genetykę',
+                'update' => 'Aktualizacja',
+            ]; @endphp
+            @foreach($tabs as $key => $label)
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link @if($vm->activeTab === $key) active @endif" data-bs-toggle="tab" data-bs-target="#tab-{{ $key }}" type="button" role="tab">{{ $label }}</button>
+                </li>
+            @endforeach
+        </ul>
 
-    <div class="tab-content">
-        @include('admin.settings.tabs.categories', ['vm' => $vm])
-        @include('admin.settings.tabs.types', ['vm' => $vm])
-        @include('admin.settings.tabs.genes', ['vm' => $vm])
-        @include('admin.settings.tabs.traits', ['vm' => $vm])
-        @include('admin.settings.tabs.winter', ['vm' => $vm])
-        @include('admin.settings.tabs.system', ['vm' => $vm])
-        @include('admin.settings.tabs.feeds', ['vm' => $vm])
-        @include('admin.settings.tabs.finance-categories', ['vm' => $vm])
-        @include('admin.settings.tabs.color-groups', ['vm' => $vm])
-        @include('admin.settings.tabs.ewelink-devices', ['vm' => $vm])
-        @include('admin.settings.tabs.genetics-generator', ['vm' => $vm, 'generatedRows' => $generatedRows ?? [], 'selectedAnimalIds' => $selectedAnimalIds ?? null])
-        @include('admin.settings.tabs.update', ['updatePanel' => $updatePanel ?? []])
+        <div class="tab-content">
+            @include('admin.settings.tabs.categories', ['vm' => $vm])
+            @include('admin.settings.tabs.types', ['vm' => $vm])
+            @include('admin.settings.tabs.genes', ['vm' => $vm])
+            @include('admin.settings.tabs.traits', ['vm' => $vm])
+            @include('admin.settings.tabs.winter', ['vm' => $vm])
+            @include('admin.settings.tabs.system', ['vm' => $vm])
+            @include('admin.settings.tabs.feeds', ['vm' => $vm])
+            @include('admin.settings.tabs.finance-categories', ['vm' => $vm])
+            @include('admin.settings.tabs.color-groups', ['vm' => $vm])
+            @include('admin.settings.tabs.ewelink-devices', ['vm' => $vm])
+            @include('admin.settings.tabs.genetics-generator', ['vm' => $vm, 'generatedRows' => $generatedRows ?? [], 'selectedAnimalIds' => $selectedAnimalIds ?? null])
+            @include('admin.settings.tabs.update', ['updatePanel' => $updatePanel ?? []])
+        </div>
     </div>
 @endsection
